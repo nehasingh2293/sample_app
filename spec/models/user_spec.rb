e0af608f5c87e95a34501b177972e394f26ec2d3
Neha Singh <neha.singh2293@gmail.com>
2012-05-03 12:00:08 -0400

    Add user micropost

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,8 @@ describe User do
 	it {should respond_to(:password_confirmation)}
 	it {should respond_to(:password_digest)}
 	it {should respond_to(:remember_token)}
-	it {should respond_to(:authenticate)}			
+	it {should respond_to(:authenticate)}	
+	it {should respond_to(:microposts)}				
 	it {should be_valid}
 	it { should respond_to(:admin) }
 	it { should_not be_admin }
@@ -93,6 +94,30 @@ describe User do
 		before { @user.save }
 		its(:remember_token) { should_not be_blank }
 	end
+	
+	describe "micropost associations" do
+
+		before { @user.save }
+		let!(:older_micropost) do 
+		  FactoryGirl.create(:micropost, :user => @user, :created_at => 1.day.ago)
+		end
+		let!(:newer_micropost) do
+		  FactoryGirl.create(:micropost, :user => @user, :created_at => 1.hour.ago)
+		end
+
+		it "should have the right microposts in the right order" do
+		  @user.microposts.should == [newer_micropost, older_micropost]
+		end
+		
+		it "should destroy associated microposts" do
+		  microposts = @user.microposts
+		  @user.destroy
+		  microposts.each do |micropost|
+		    Micropost.find_by_id(micropost.id).should be_nil
+        end
+    end
+  end
+end
 end
 
 
