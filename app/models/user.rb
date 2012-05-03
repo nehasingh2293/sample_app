ddd317fafa1c10349f02091014c389cd7f29fdb6
Neha Singh <neha.singh2293@gmail.com>
2012-05-03 10:38:32 -0400

    Finish user edit, delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ActiveRecord::Base
   private
 
     def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
+      self.remember_token = SecureRandom.base64
     end
 end
